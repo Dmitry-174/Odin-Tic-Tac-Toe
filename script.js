4097e3fa6c747ce442b56704405b20bf209fcb98
Dmitrii <dmitry.valiev174@gmail.com>
2022-11-07 22:40:25 +0500

    Make renderGameBoard function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict"
 
 const gameBoard = (() => {
-    let board = new Array(9);
+    //let board = new Array(9);
+
+    let board = ['X', '0', 'X',
+                '0', 'X', '0',
+                'X', '0', 'X'];
 
     const getMark = (index) => {
         return board[index];
@@ -11,9 +15,20 @@ const gameBoard = (() => {
         board[index] = typeMark;
     }
 
-    return {getMark, setMark}
+    return { getMark, setMark }
 })();
 
-console.dir(gameBoard.board);
-console.dir(gameBoard.setMark(1, 'X'));
-console.dir(gameBoard.getMark(1));
+
+function renderGameBoard() {
+    let gameBoardEl = document.querySelector('.game-board')
+    for (let i = 0; i < 9; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('id', i);
+        cell.textContent = gameBoard.getMark(i);
+        gameBoardEl.appendChild(cell);
+    }
+}
+
+
+renderGameBoard();
